6a34dcc4a799e6a3edbe36ee771b8cda93c18f7b
bernardooi <83948090+bernardooi@users.noreply.github.com>
2022-10-25 11:04:35 +0200

    products click

--- static/script/kassa.js
+++ static/script/kassa.js
@@ -2,16 +2,33 @@
 $.get("/db/get_items").done(function (data){
     Object.keys(data[1].items).forEach(e=>{
         console.log(data[1].items[e])
-
         var info = data[1].items[e];
         products(info);
+    })
+
+    $(".produkter").click(function(){
+        let product_id = this.getAttribute("product_id");
+
+        
+    })
+
+    data[1].categorys.forEach(e=>{
+        console.log(e)
+        category(e);
     })
 })
 
 
-function products(data){
+let category=(kategori)=>{
+    var div = $("<div>").attr({"class":"sortobject"});
+    var name = $("<h3>").html(kategori);
+    div.append(name);
+    $("#nav").append(div);
+}
 
-    var div = $("<div>").attr({"class":"produkter", "onclick": `kundvagn(${data})`});
+let products=(data)=>{
+    console.log(data);
+    var div = $("<div>").attr({"class":"produkter", "product_id": data.product_id});
     var img = $("<img>").attr({"src": data.image_src, "class":"itemImage"});
     var name = $("<p>").attr({"class":"productname"}).add(`<span>${data.display_name}</span>`);
     div.append(name);
@@ -22,6 +39,7 @@ function products(data){
 
 let kundvagn = (id) => 
 {
+    Object.keys()
     let arr=[];
     arr+=id;
     console.log(arr);
