29ce1f706cea9beab53de57c92a6f2fd88939182
Tobahman23 <tobias23.ta@gmail.com>
2022-10-26 13:11:10 +0200

    e
    e

---
+++ static/script/db.js
@@ -0,0 +1,58 @@
+var vagn={
+
+}
+//hämtar från databasen och definerar variabler.
+//main funktion
+api("/db/get_items", {}, data=>{
+    Object.keys(data[1].items).forEach(e=>{
+        vagn[e]=0;
+        var info = data[1].items[e];
+        allproducts(info);
+    })
+
+    $(".produkter").click(function(){
+        let product_id = this.getAttribute("product_id");
+        kundvagn(product_id)
+        
+    })
+
+    data[1].categorys.forEach(e=>{
+        console.log(e)
+        category(e);
+    })
+
+    //Visar endast produkter som är i den valda kategorin
+   $(".sortobject").click(function(){
+    if (this.getAttribute("category") === "Food"){
+        $(".Food").show();
+        $(".Drinks").hide();
+    }
+    else if (this.getAttribute("category") === "Drinks"){
+        $(".Food").hide();
+        $(".Drinks").show();
+    }
+    else if (this.getAttribute("category") === "All"){
+        $(".Food").show();
+        $(".Drinks").show();
+    }
+    })
+})
+
+//lägger till kategorierna från databasen
+let category=(kategori)=>{
+    var div = $("<div>").attr({"class":"sortobject","category": kategori});
+    var name = $("<h3>").html(kategori);
+    div.append(name);
+    $("#nav").append(div);
+}
+// visar allting på content
+let allproducts=(data)=>{
+
+    var div = $("<div>").attr({"class":"produkter " + `${data.category}`,"product_id": data.product_id});
+    var img = $("<img>").attr({"src": data.image_src, "class":"itemImage"});
+    var name = $("<p>").attr({"class":"productname"}).add(`<h1>${data.display_name}</h1>`);
+    div.append(name);
+    div.append(img);
+    $("#content").append(div);
+
+}
