e85790915a2bcae8d83f5c1657b941c4f695c0ab
MfeMeaf <80445981+MfeMeaf@users.noreply.github.com>
2022-11-08 14:50:53 +0100

    bought Month
    skriver nu ut ID + mängd kr det kostade....
    
    Den kommenterade koden är för sen när det finns en path till products_bought.json

--- static/script/db.js
+++ static/script/db.js
@@ -1,3 +1,96 @@
+// Hämtar dagens datum och delar upp det till År Månad och Dag
+var date1 = new Date();
+var year = date1.getFullYear();
+var month = date1.getMonth() +1;
+// padStart Lägger till variabel / sträng "0" tills det uppnår den specificerade längden "2"
+var day = String(date1.getDate()).padStart(2,'0');
+// Temp variabel
+var boti = 0;
+const boughtdata =[
+    {
+        "date_of_transaction": "2022-11-08 10:54:14",
+        "products_bought": {
+            "1200303": 1
+        },
+        "price_paid": {
+            "total_money_in": 50.0,
+            "notations_changed": {
+                "50": -1,
+                "100": -4,
+                "500": 1
+            }
+        }
+    },
+    {
+        "date_of_transaction": "2022-11-08 10:54:48",
+        "products_bought": {
+            "1949812": 3
+        },
+        "price_paid": {
+            "total_money_in": 300.0,
+            "notations_changed": {
+                "100": -2,
+                "500": 1
+            }
+        }
+    },
+    {
+        "date_of_transaction": "2022-11-08 11:03:45",
+        "products_bought": {
+            "1200303": 1,
+            "1312111": 1
+        },
+        "price_paid": {
+            "total_money_in": 70.0,
+            "notations_changed": {
+                "10": -1,
+                "20": -1,
+                "100": 1
+            }
+        }
+    },
+    {
+        "date_of_transaction": "2022-11-08 11:10:21",
+        "products_bought": {
+            "1313125": 1,
+            "1415901": 1,
+            "1581811": 1
+        },
+        "price_paid": {
+            "total_money_in": 58.0,
+            "notations_changed": {
+                "2": -1,
+                "20": -2,
+                "100": 1
+            }
+        }
+    },
+    {
+        "date_of_transaction": "2022-11-08 11:11:36",
+        "products_bought": {},
+        "price_paid": {
+            "total_money_in": 0,
+            "notations_changed": {}
+        }
+    },
+    {
+        "date_of_transaction": "2022-11-08 11:11:44",
+        "products_bought": {
+            "1312111": 1,
+            "1415901": 1,
+            "1581811": 1
+        },
+        "price_paid": {
+            "total_money_in": 68.0,
+            "notations_changed": {
+                "2": -1,
+                "10": -1,
+                "20": -1,
+                "100": 1
+            }
+        }
+    }
+]
 var vagn={
 
 }
@@ -31,7 +124,6 @@ api("/db/get_items", {}, data=>{
         allproducts(info);
     })
     data[1].categorys.forEach(e=>{
-        console.log(e)
         category(e);
     })
     //Visar endast produkter som är i den valda kategorin
@@ -41,30 +133,46 @@ api("/db/get_items", {}, data=>{
         $(".Drinks").hide();
         $(".Snacks").hide();
         $(".Others").hide();
+        $(".BoughtMonth").hide();
     }
     else if (this.getAttribute("category") === "Drinks"){
         $(".Food").hide();
         $(".Drinks").show();
         $(".Snacks").hide();
         $(".Others").hide();
+        $(".BoughtMonth").hide();
     }
     else if (this.getAttribute("category") === "All"){
         $(".Food").show();
         $(".Drinks").show();
         $(".Snacks").show();
         $(".Others").show();
+        $(".BoughtMonth").hide();
     }
     else if (this.getAttribute("category") === "Snacks"){
         $(".Food").hide();
         $(".Drinks").hide();
         $(".Snacks").show();
         $(".Others").hide();
+        $(".BoughtMonth").hide();
     }
     else if (this.getAttribute("category") === "Others"){
         $(".Food").hide();
         $(".Drinks").hide();
         $(".Snacks").hide();
         $(".Others").show();
+        $(".BoughtMonth").hide();
+        }
+    else if (this.getAttribute("category") === "BoughtMonth"){
+        $(".Food").hide();
+        $(".Drinks").hide();
+        $(".Snacks").hide();
+        $(".Others").hide();
+        $(".BoughtMonth").show();
+        if(boti == 0)
+        BoughtMonth(year,month,boughtdata);
+        
+        boti++;
     }
     })
     //När submitbutton klickas så tar denna funktion values för inputs
@@ -88,7 +196,6 @@ api("/db/get_items", {}, data=>{
             "product_id": numone + prodid,
             "item_count": amount
         }, data=>{
-            console.log(data);
             if (data[0] != 0){
                 alert(data);
             }
@@ -138,37 +245,42 @@ let allproducts=(data)=>{
     $("#content").append(div);
 }
 
-// Hämtar dagens datum och delar upp det till År Månad och Dag
-var date1 = new Date();
-var year = date1.getFullYear();
-var month = date1.getMonth() +1;
-// padStart Lägger till variabel / sträng "0" tills det uppnår den specificerade längden "2"
-var day = String(date1.getDate()).padStart(2,'0');
 
-api("/db/purchase_history", {}, data=>{
-    console.log(data)
-    // Jämnför månad och skriver ut allt som säldes under den månaden
-    let boughtMonth = (year,month) =>{
+let BoughtMonth = (year,month,boughtdata) =>{
     // Lägger ihopp År och Månad med - mellan
     var fullDate = [year,month].join("-");
-    Object.keys(data).forEach(i => {
-        if(fullDate == date[i].date_of_transaction.slice(0,-3))
+    Object.keys(boughtdata).forEach(i => {
+        var boughtProd = "";
+        if(fullDate == boughtdata[i].date_of_transaction.slice(0,-12))
         {
-            console.log(data[i].date_of_transaction);
-            console.log(data[i].products_bought);
-            console.log(data[i].price_paid.total_money_in);
+            Object.keys(boughtdata[i].products_bought).forEach(i => {
+                boughtProd = boughtProd.concat(i, ",");
+            });
+            var name = $("<p>").attr({"class":"productname BoughtMonth"}).html(`${boughtProd}`);
+            var price = $("<p>").attr({"class":"price BoughtMonth"}).html(`${boughtdata[i].price_paid.total_money_in} kr`);
+            var br = $("<br>");
+            $("#content").append(name,price,br);
         }
-    });}
+    })
+};
+// api("/db/purchase_history", {}, data=>{
+//     // console.log(data)
 
-    // Samma som functionen ovan men jämnför dagen också.
-    let boughtDay = (year,month,day) =>{
-    var fullDate = [year,month,day].join("-");
-    Object.keys(data).forEach(i => {
-        if(fullDate == data[i].date_of_transaction)
-        {
-            console.log(data[i].date_of_transaction);
-            console.log(data[i].products_bought);
-            console.log(data[i].price_paid.total_money_in);
-        }
-    });}
-})
+//     let BoughtMonth = (year,month,boughtdata) =>{
+    // Lägger ihopp År och Månad med - mellan
+//     var fullDate = [year,month].join("-");
+//     Object.keys(boughtdata).forEach(i => {
+//         var boughtProd = "";
+//         if(fullDate == boughtdata[i].date_of_transaction.slice(0,-12))
+//         {
+//             Object.keys(boughtdata[i].products_bought).forEach(i => {
+//                 boughtProd = boughtProd.concat(i, ",");
+//             });
+//             var name = $("<p>").attr({"class":"productname BoughtMonth"}).html(`${boughtProd}`);
+//             var price = $("<p>").attr({"class":"price BoughtMonth"}).html(`${boughtdata[i].price_paid.total_money_in} kr`);
+//             var br = $("<br>");
+//             $("#content").append(name,price,br);
+//         }
+//     })
+// };
+// })
